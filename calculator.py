35977655ddc0543577835d9cd0755e97a87e8e53
mdkallick <need4huskys@gmail.com>
2014-04-21 15:15:50 -0400

    concatenation commit
    We figured out how to get the label to have a multi-digit display and
    got the value variable which keeps track of the display to reset each
    time an operation key is pressed. We also changed test2 to be a
    different test, one that we will probably end up using as our way of
    adding numbers.

--- calculator.py
+++ calculator.py
@@ -5,23 +5,31 @@ win.title('Calculator')
 # Will hold the display of the calculator
 textbox = StringVar()
 textbox.set("")
+value = ""
+global value #initial value of variable value
 
 def buttonClicked(event):
+    global value #calling value of value into local function
     button = event.widget
-    value = button.cget("text") # cget recognizes the text of the button!
+    value = value + button.cget("text") # cget recognizes the text of the button, and + allows us to make multi-digit numbers
     textbox.set(value)
+    return value
+
+def functionClicked(event):
+    button = event.widget
+    item = button.cget("text")
+    textbox.set(item)
+    var1 = value
+    value = ""
+    global value #clears previous entry
 
 
-""" #this is the long form of the below array, if we have to resort to it
-b1 = Button(win, text="1")
-b1.pack()
-b1.bind("<Button-1>", buttonClicked)
 
-b2 = Button(win, text="2")
-b2.pack()
-b2.bind("<Button-1>", buttonClicked)
-"""
 
+
+
+    
+    
 buttons = []  # holds all buttons
 for buttonNum in range(0,10):
     # creates a new button with that number
@@ -32,43 +40,21 @@ for buttonNum in range(0,10):
     buttons.append(button)
     button.grid(row=int(buttonNum/3)+1 , column=buttonNum % 3) 
 
-# need to handle the non-numeric buttons
-# how to loop over some characters instead of numbers?
 
-
-functions = ["%","*","-","+","="]
+funclist = ["%","*","-","+","="]
+functions = []
 for operator in range(0,5):
-    button = Button(win, text=str(functions[operator]))
+    button = Button(win, text=str(funclist[operator]))
     button.pack()
-    button.bind("<Button-1>", buttonClicked)
+    button.bind("<Button-1>", functionClicked)
     functions.append(button)
-    # print (functions[operator])
     button.grid(row=int(operator+1) , column=4)
     
-"""
-bdiv = Button(win, text="/")
-bmul = Button(win, text="*")
-badd = Button(win, text="+")
-bsub = Button(win, text="-")
-bequ = Button(win, text="=")
-bclr = Button(win, text='c')
-b1.grid(row=3, column=1)
-b2.grid(row=3, column=2)
-b3.grid(row=3, column=3)
-b4.grid(row=2, column=1)
-b5.grid(row=2, column=2)
-b6.grid(row=2, column=3)
-b7.grid(row=1, column=1)
-b8.grid(row=1, column=2)
-b9.grid(row=1, column=3)
-b0.grid(row=4, column=1)
-bdiv.grid(row=1, column=4)
-bmul.grid(row=2, column=4)
-badd.grid(row=4, column=4)
-bsub.grid(row=3, column=4)
-bequ.grid(row=4, column=3)
-bclr.grid(row=4, column=2)
-"""
+#want to create a dictionary to hold previous values, which can then be accessed as a whole with operator(s)when equal sign is pressed
+#need to def funcs for all operators
+
+    
+
 l = Label(win, textvariable=textbox)
 l.grid(row=0, column=2)
 
---
+++ test2.py
@@ -0,0 +1,7 @@
+number1 = 1
+number2 = 2
+def plus(num1, num2):
+    final = num1 + num2
+    print (final)
+plus(number1, number2)
+
